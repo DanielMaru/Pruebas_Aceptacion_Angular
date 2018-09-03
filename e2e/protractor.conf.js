5c4c7f598c1720ea494a835d9248af089d1e73f8
Daniel Marulanda <danielfe95@hotmail.com>
2018-09-03 15:09:36 -0500

    prueba

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -6,23 +6,25 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './src/features/*.features'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {    
+    require: ['./src/step-definitions/*.steps.ts'],
+    tags: [],    
+    dryRun: false,
+    compiler: []
+    },   
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    
   }
 };
